9331345c85297420187d69552fcaf0451b00176f
Corey Mosher <corey.mosher@gmail.com>
2016-07-03 22:04:21 -0300

    Initial Commit

---
+++ Reconcyl/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Reconcyl
+//
+//  Created by Corey Mosher on 2016-07-03.
+//  Copyright © 2016 Corey Mosher. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Reconcyl/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Reconcyl
+//
+//  Created by Corey Mosher on 2016-07-03.
+//  Copyright © 2016 Corey Mosher. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
